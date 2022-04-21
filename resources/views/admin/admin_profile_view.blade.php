44943bf20c789943477fd84ef6c81219842055d6
Asaduzzaman <ajaman375@gmail.com>
2022-04-21 12:36:07 -0700

    admin profile eiditing

--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -9,11 +9,12 @@
                 <div class="box box-widget widget-user">
                     <!-- Add the bg color to the header using any of the bg-* classes -->
                     <div class="widget-user-header bg-black">
-                        <h3 class="widget-user-username">Admin Name: </h3>
-                        <h6 class="widget-user-desc">Admin Email:  </h6>
+                        <h3 class="widget-user-username">Admin Name: {{ $adminData->name }} </h3>
+                        <a href="" style="float: right;" class="btn btn-rounded btn-success">Edit Profile</a>
+                        <h6 class="widget-user-desc">Admin Email: {{ $adminData->email }} </h6>
                     </div>
                     <div class="widget-user-image">
-                        <img class="rounded-circle" src="{{ asset('backend/images/user3-128x128.jpg') }}" alt="User Avatar">
+                        <img class="rounded-circle" src="{{ (!empty($adminData->profile_photo_path))?url('upload/admin_images/'.$adminData->profile_photo_path) :url('upload/no_image.jpg') }}" alt="User Avatar">
                     </div>
                     <div class="box-footer">
                         <div class="row">
